6381fae8c6596af0940aa76325cccb6c9abb67ec
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 22:17:40 +0200

    SnowMouse constructor

--- src/Pics/SnowMouse.cs
+++ src/Pics/SnowMouse.cs
@@ -1,9 +1,11 @@
 namespace Pics {
     public class SnowMouse : Mouse
     {
-        public SnowMouse() : base(Ground.Snow)
+        public SnowMouse(Coordinates coordinates, int speed, Direction direction) : base(Ground.Snow)
         {
-            //
+            this.coordinates = coordinates;
+            this.speed = speed;
+            this.direction = direction;
         }
     }
 }
