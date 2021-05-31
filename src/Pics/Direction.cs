cc30897f91afeb219ab3f7ebe9a3a7bda0e7e98b
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 22:17:38 +0200

    add flip direction

--- src/Pics/Direction.cs
+++ src/Pics/Direction.cs
@@ -1,7 +1,25 @@
+using System;
+
 namespace Pics {
     public enum Direction {
         UpLeft, Up, UpRight,
         Left, Stop, Right,
         DownLeft, Down, DownRight
+    }
+    static class DirectionExtensions {
+        public static Direction flip (this Direction input) {
+            switch(input) {
+                case Direction.UpLeft: return Direction.DownRight;
+                case Direction.Up: return Direction.Down;
+                case Direction.UpRight: return Direction.DownLeft;
+                case Direction.Left: return Direction.Right;
+                case Direction.Right: return Direction.Left;
+                case Direction.DownLeft: return Direction.UpRight;
+                case Direction.Down: return Direction.Up;
+                case Direction.DownRight: return Direction.UpLeft;
+            }
+            throw new ArgumentException($"Unknown Direction to flip: {input}");
+        }
+
     }
 }
