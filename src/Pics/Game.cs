5063f786331cae3110d77d7e767897d48f103f5e
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 22:17:37 +0200

    run game_loop

--- src/Pics/Game.cs
+++ src/Pics/Game.cs
@@ -16,29 +16,49 @@ namespace Pics
             var r = new Room(config.width, config.height);
             Console.WriteLine("Room:");
             Console.WriteLine(r);
+            game_loop();
+            Console.WriteLine("Done.");
         }
 
         private void prepare_new_game() {
             set_initial_level();
         }
 
+        private void set_initial_level() {
+            level = 1;
+        }
+
+        private void increase_level() {
+            level++;
+        }
+
+        private bool is_insane_level() {
+            return level > 10;
+        }
         private void game_loop() {
-            while(true) {
+            bool keep_playing = true;
+            do {
                 var result = play_level();
                 switch(result) {
-                    case next_level:
+                    case LevelResult.next_level:
                         increase_level();
+                        if(is_insane_level()) {
+                            keep_playing = false;
+                            Console.WriteLine("Too much.");
+                        }
                         break;
-                    case game_over:
+                    case LevelResult.game_over:
                         prepare_new_game();
                         break;
-                    case quit:
-                        return;
+                    case LevelResult.quit:
+                        keep_playing = false;
+                        break;
                 }
-            }
+            } while (keep_playing);
         }
 
         private LevelResult play_level() {
+            return LevelResult.next_level;
         }
 
     }
