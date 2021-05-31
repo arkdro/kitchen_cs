1160ee50e3c156effd48d3b82075c8df2a557d4a
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 22:17:37 +0200

    add cell string representation

--- src/Pics/Cell.cs
+++ src/Pics/Cell.cs
@@ -1,8 +1,21 @@
+using System;
+
 namespace Pics
 {
     public enum Cell {
         Ground,
         Food,
         Animal
+    }
+    static class Extensions {
+        public static string CellName(this Cell input) {
+            switch(input) {
+                case Cell.Ground: return ".";
+                case Cell.Food: return "o";
+                case Cell.Animal: return "X";
+            }
+            throw new ArgumentException($"Unknown Cell value: {input}");
+        }
+
     }
 }
