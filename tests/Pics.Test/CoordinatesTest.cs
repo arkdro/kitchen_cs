edd75e21c171d79eb38dca66a50977839a5d6f85
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 22:17:39 +0200

    add a test

--- tests/Pics.Test/CoordinatesTest.cs
+++ tests/Pics.Test/CoordinatesTest.cs
@@ -16,5 +16,15 @@ namespace Pics.Test
             var expected = true;
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void EqualityTest()
+        {
+            var c1 = new Coordinates(x: 1234, y: 5432);
+            var c2 = new Coordinates(x: 1234, y: 5432);
+            var actual = c1 == c2;
+            var expected = true;
+            Assert.Equal(expected, actual);
+        }
     }
 }
