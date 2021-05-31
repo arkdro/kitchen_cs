c92cb0e38b6d4ffd919c4825d5e3e23b90c1abfd
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 22:17:39 +0200

    KeyboardInput

---
+++ src/Pics/KeyboardInput.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading;
+
+namespace Pics {
+    public class KeyboardInput {
+        public static ConsoleKey? get_input() {
+            ConsoleKey key = ConsoleKey.Spacebar;
+            Thread.Sleep(1);
+            if(Console.KeyAvailable) {
+                key = Console.ReadKey(true).Key;
+                return key;
+            }
+            return null;
+        }
+
+        public static Direction get_direction(Direction old) {
+            var key = get_input();
+            if(key is null) {
+                return old;
+            }
+            switch (key) {
+                case ConsoleKey.UpArrow:
+                    return Direction.Up;
+                case ConsoleKey.DownArrow:
+                    return Direction.Down;
+                case ConsoleKey.LeftArrow:
+                    return Direction.Left;
+                case ConsoleKey.RightArrow:
+                    return Direction.Right;
+                default:
+                    return Direction.Stop;
+            }
+        }
+    }
+}
