d733189223545f89808afd9db08109111601b39a
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 22:17:37 +0200

    add a config

---
+++ src/Pics/Config.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Configuration;
+
+namespace Pics
+{
+    class Config
+    {
+        public readonly int width;
+        public readonly int height;
+        public Config(string config_file)
+        {
+            try
+            {
+                var setting = ConfigurationManager.AppSettings;
+                width = get_number(setting, "width", "10");
+                height = get_number(setting, "height", "10");
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("config file error");
+                throw;
+            }
+
+        }
+        private int get_number(System.Collections.Specialized.NameValueCollection settings, string key, string default_value) {
+            var value = settings[key] ?? default_value;
+            return int.Parse(value);
+        }
+    }
+}
