c458a8b3516cb7843517cf5d94fb12c4fe791f16
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 22:17:39 +0200

    can_bounce_horisontal_wall

--- src/Pics/Mouse.cs
+++ src/Pics/Mouse.cs
@@ -101,6 +101,13 @@ namespace Pics {
             return new_cell == preferred_ground;
         }
 
+        private bool can_bounce_horisontal_wall(Room room) {
+            var new_direction = direction.flip_horizontal_wall();
+            var new_coordinates = Move.move(new_direction, coordinates);
+            var new_cell = room.get(new_coordinates);
+            return new_cell == preferred_ground;
+        }
+
         private void stop() {
             direction = Direction.Stop;
             speed = 0;
