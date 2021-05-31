76b46f913843bdec8a88f4a213c2ee2c451fc639
Arkadiy Drovosekov <arkdro@gmail.com>
2021-05-31 22:17:37 +0200

    add a game_loop stub

--- src/Pics/Game.cs
+++ src/Pics/Game.cs
@@ -4,7 +4,9 @@ namespace Pics
 {
     class Game
     {
+        private int level;
         private Config config;
+
         public Game() {
             config = new Config();
         }
@@ -19,6 +21,23 @@ namespace Pics
         private void prepare_new_game() {
             set_initial_level();
         }
+
+        private void game_loop() {
+            while(true) {
+                var result = play_level();
+                switch(result) {
+                    case next_level:
+                        increase_level();
+                        break;
+                    case game_over:
+                        prepare_new_game();
+                        break;
+                    case quit:
+                        return;
+                }
+            }
+        }
+
         private LevelResult play_level() {
         }
 
